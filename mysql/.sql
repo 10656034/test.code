0b2d0beaa3aa1d52f0804912c066361d9fd05be6
10656034 <59244658+10656034@users.noreply.github.com>
2021-12-17 02:55:49 +0800

    sql語句測試

---
+++ mysql/.sql
@@ -0,0 +1,11 @@
+SELECT * FROM zabbix.sensor where Node_sensor='N01S01' order by Record_time desc limit 1
+
+SELECT * FROM zabbix.sensor where Node_sensor='N01S02' order by Record_time desc limit 1
+
+SELECT * FROM zabbix.sensor where Node_sensor='N02S01' order by Record_time desc limit 1
+
+SELECT * FROM zabbix.sensor where Node_sensor='N02S02' order by Record_time desc limit 1
+
+SELECT * FROM zabbix.sensor where Node_sensor='N03S01' order by Record_time desc limit 1
+
+SELECT * FROM zabbix.sensor where Node_sensor='N03S02' order by Record_time desc limit 1
